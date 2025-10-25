6bba7c3be21de3af81962148371fc009adc07118
Alec Savvy <alecsavvy@outlook.com>
2025-10-25 15:37:12 -0600

    status check

--- pkg/commands/status.go
+++ pkg/commands/status.go
@@ -1,33 +1,56 @@
 package commands
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"net"
 
+	"connectrpc.com/connect"
+	v1 "github.com/OpenAudio/go-openaudio/pkg/api/core/v1"
+	"github.com/OpenAudio/go-openaudio/pkg/sdk"
 	"github.com/spf13/cobra"
 )
 
 func NewStatusCmd() *cobra.Command {
 	var endpoint string
+	var socket string
 
 	cmd := &cobra.Command{
 		Use:   "status",
 		Short: "Query the status of a running OpenAudio node",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if endpoint == "" {
-				endpoint = "/tmp/openaudio.sock" // or detect from config
+			if endpoint != "" && socket != "" {
+				return errors.New("cannot specify both --endpoint and --socket")
 			}
 
-			if _, err := net.Dial("unix", endpoint); err == nil {
-				fmt.Printf("Connected to OpenAudio node via UNIX socket: %s\n", endpoint)
-			} else {
-				fmt.Printf("Could not connect via UNIX socket, trying ConnectRPC: %s\n", endpoint)
-				// TODO: Connect to ConnectRPC endpoint here
+			var oap *sdk.OpenAudioSDK
+			switch {
+			case endpoint != "":
+				oap = sdk.NewOpenAudioSDK(endpoint)
+			case socket != "":
+				oap = sdk.NewOpenAudioSDK(socket)
+			default:
+				return errors.New("must specify either --endpoint or --socket")
 			}
-			return nil
+
+			res, err := oap.Core.GetStatus(cmd.Context(), &connect.Request[v1.GetStatusRequest]{})
+			if err != nil {
+				return err
+			}
+
+			msg := res.Msg
+			data, err := json.MarshalIndent(msg, "", "  ")
+			if err != nil {
+				return fmt.Errorf("failed to marshal response: %w", err)
+			}
+
+			_, err = cmd.OutOrStdout().Write(data)
+			return err
 		},
 	}
 
-	cmd.Flags().StringVar(&endpoint, "endpoint", "", "Node endpoint (e.g., validator1.openaudio.org or local UNIX socket)")
+	cmd.Flags().StringVar(&endpoint, "endpoint", "", "Node endpoint (e.g. https://validator1.openaudio.org)")
+	cmd.Flags().StringVar(&socket, "socket", "", "Local UNIX socket path to the node")
+
 	return cmd
 }
